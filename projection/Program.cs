58bedfbfe2f0656864724e67992520e62fe8aa52
kimserey <kimserey.lam@gmail.com>
2020-03-26 09:55:31 +0000

    Added committer

--- projection/Program.cs
+++ projection/Program.cs
@@ -1,25 +1,127 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Mime;
 using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using Newtonsoft.Json;
 using SqlStreamStore;
 using SqlStreamStore.Streams;
 using sqlstreamstore_tests;
 
 namespace projection
 {
+    /// CREATE TABLE Ledger (Name varchar(255), Position bigint, PRIMARY KEY (Name))
+    public class Committer
+    {
+        public class LedgerEntry
+        {
+            public string Name { get; set; }
+            public long Position { get; set; }
+        }
+
+        private readonly string _name;
+        private readonly string _connectionString;
+
+        public Committer(string name, string connectionString)
+        {
+            _name = name;
+            _connectionString = connectionString;
+        }
+
+        public long Init()
+        {
+            long position = -1;
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute(
+                    @"
+                        IF NOT EXISTS (SELECT * FROM Ledger WHERE Name = @name)
+                            INSERT INTO Ledger (Name, Position)
+                            VALUES (@name, @position)
+                    ",
+                    new { name = _name, position = -1 }
+                );
+
+                position = connection
+                    .Query<LedgerEntry>(
+                        "SELECT * FROM Ledger WHERE Name = @name",
+                        new {name = _name})
+                    .First()
+                    .Position;
+            }
+
+            return position;
+        }
+
+        public void CommitPosition(long position)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute(
+                    "UPDATE Ledger SET Position = @position WHERE Name = @name;",
+                    new { position, name = _name }
+                );
+            }
+        }
+    }
+
+    public class Projection
+    {
+        private readonly MsSqlStreamStore _store;
+        private readonly Committer _committer;
+
+        public Projection()
+        {
+            var connString = "Server=localhost;Database=eqx;User Id=sa;Password=...;";
+            _store =  new MsSqlStreamStore(new MsSqlStreamStoreSettings(connString));
+            _committer = new Committer("MyProjection", connString);
+        }
+
+        private async Task StreamMessageReceived(IAllStreamSubscription subscription, StreamMessage streamMessage,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var message = await streamMessage.GetJsonData(cancellationToken);
+                Console.WriteLine(
+                    $"Start processing: type:{streamMessage.Type}, positon:{streamMessage.Position}, stream version:{streamMessage.StreamVersion}, {message}");
+                Thread.Sleep(1000);
+                Console.WriteLine("Done");
+
+                _committer.CommitPosition(streamMessage.Position);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private void HasCaughtUp(bool hasCaughtUp)
+        {
+            Console.WriteLine($"Has caught up {hasCaughtUp}");
+        }
+
+        public void Start()
+        {
+            var position = _committer.Init();
+            _store.SubscribeToAll(position, StreamMessageReceived, hasCaughtUp: HasCaughtUp);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var streamId = new StreamId("Account:93aad7b7-f6d2-438f-911b-8eaba5695d48");
-            var store = new MsSqlStreamStore(new MsSqlStreamStoreSettings("Server=localhost;Database=sqlstream;User Id=sa;Password=p@ssw0rd;"));
-            var balanceProjection = new BalanceProjection(store, streamId);
+            var proj = new Projection();
+            proj.Start();
 
-            while (true)
-            {
-                Thread.Sleep(1000);
-            }
-
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadLine();
         }
     }
 }
--- sqlstreamstore-tests/Program.cs
+++ sqlstreamstore-tests/Program.cs
@@ -14,7 +14,7 @@ namespace sqlstreamstore_tests
         static async Task Main()
         {
             var streamId = new StreamId("Account:93aad7b7-f6d2-438f-911b-8eaba5695d48");
-            var store = new MsSqlStreamStore(new MsSqlStreamStoreSettings("Server=localhost;Database=sqlstream;User Id=sa;Password=p@ssw0rd;"));
+            var store = new MsSqlStreamStore(new MsSqlStreamStoreSettings("Server=localhost;Database=eqx;User Id=sa;Password=p@ssw0rd;"));
             store.CreateSchema().Wait();
             _streamStore = store;
             _account = new Account(_streamStore, streamId);
